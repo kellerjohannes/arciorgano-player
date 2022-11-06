f3acb2c97d6ee10ae2567ba4ba35ad90e90dc416
Johannes Keller <johannes.keller@mab-bs.ch>
2022-11-06 14:07:46 +0100

    working on mode3 navigator

--- lisp/mode3-navigator/navigator.lisp
+++ lisp/mode3-navigator/navigator.lisp
@@ -10,12 +10,14 @@
 (rt-start)
 
 (defun key-off (index)
-  (when (< 0 index 147)
+  "Switches off the magnet with `index' by sending an OSC message to the PureData-Patch."
+  (when (and index (< 0 index 147))
     (format t "~a:off " index)
     (osc:message *osc-out* "/incudine-bridge" "ii" index 0)))
 
 (defun key-on (index &optional duration-in-sec)
-  (when (< 0 index 147)
+  "Switches on the magnet with `index' by sending an OSC message to the PureData-Patch."
+  (when (and index (< 0 index 147))
     (osc:message *osc-out* "/incudine-bridge" "ii" index 1)
     (format t "~a:on " index)
     (when duration-in-sec
@@ -59,9 +61,53 @@
     (b♭+ f+ d+ e♭ g♭+)
     (d+ a+ f♯+ g b♭+)
     (f♯+ c♯+ a♯+ b♮ d+)
-    (a♯+ nil nil d♯ f♯+)))
+    (a♯+ nil nil d♯ f♯+))
+  "This represents the Mode3 Tonnetz. The first symbol is considered the origin, the CDR are the neighbours: north, east, south, west.")
+
+(defparameter *pitchclass-table*
+  '((e♭ . 11)
+    (g . 22)
+    (b♮ . 34)
+    (d♯ . 9)
+    (a♭ . 26)
+    (c . 1)
+    (e . 13)
+    (g♯ . 24)
+    (b♯ . 36)
+    (d♭ . 5)
+    (f . 16)
+    (a . 28)
+    (c♯ . 3)
+    (e♯ . 15)
+    (g♭ . 20)
+    (b♭ . 32)
+    (d . 7)
+    (f♯ . 18)
+    (a♯ . 30)
+    (e♭+ . 12)
+    (g+ . 23)
+    (b♮+ . 35)
+    (d♯+ . 10)
+    (a♭+ . 27)
+    (c+ . 2)
+    (e+ . 14)
+    (g♯+ . 25)
+    (d♭+ . 6)
+    (f+ . 17)
+    (a+ . 29)
+    (c♯+ . 4)
+    (g♭+ . 21)
+    (b♭+ . 33)
+    (d+ . 8)
+    (f♯+ . 19)
+    (a♯+ . 31))
+  "Mapping between pitchclass names and key indices, required to play pitchclass information on the organ.")
+
+
+;; Navigating the Tonnetz
 
 (defun move-1 (origin direction)
+  "Returns the pitchclass name of a neighbour of `origin', based on `direction' ('north, 'east, 'south, 'west)."
   (let ((neighbours (cdr (assoc origin *network*))))
     (case direction
       (north (first neighbours))
@@ -70,6 +116,7 @@
       (west (fourth neighbours)))))
 
 (defun lookup-direction (x y)
+  "Returns a direction symbol based on relative unit coordinates."
   (let ((d (list x y)))
     (cond ((equal d '(0 0)) nil)
           ((equal d '(0 1)) 'north)
@@ -79,27 +126,80 @@
           (t nil))))
 
 (defun move (origin delta-x delta-y)
+  "Returns the pitchclass name of the neighbour of `origin' (also a pitchclass name) specified by the direction vector (`delta-x' `delta-y')."
   (cond ((null origin) nil)
-    ((and (zerop delta-x) (zerop delta-y))
-     origin)
-    ((zerop delta-x)
-     (let ((step (if (> delta-y 0) -1 1)))
-       (move (move-1 origin (lookup-direction 0 (- step))) 0 (+ delta-y step))))
-    (t (let ((step (if (> delta-x 0) -1 1)))
-       (move (move-1 origin (lookup-direction (- step) 0)) (+ delta-x step) delta-y)))))
+        ((and (zerop delta-x) (zerop delta-y))
+         origin)
+        ((zerop delta-x)
+         (let ((step (if (> delta-y 0) -1 1)))
+           (move (move-1 origin (lookup-direction 0 (- step))) 0 (+ delta-y step))))
+        (t (let ((step (if (> delta-x 0) -1 1)))
+             (move (move-1 origin (lookup-direction (- step) 0)) (+ delta-x step) delta-y)))))
 
 (defun generate-series (origin delta-x delta-y)
+  "Returns a list of pitchclass names by moving recursively from `origin' by an interval described by the vector (`delta-x' `delta-y')."
   (cond ((null origin) nil)
-    (t (format t "~&~a" origin)
-       (generate-series (move origin delta-x delta-y) delta-x delta-y))))
+        (t (cons origin (generate-series (move origin delta-x delta-y) delta-x delta-y)))))
 
-(defparameter *pitchclass-table* '((e♭ . 11) (g . 22) (b♮ . 34) (d♯ . 9) (a♭ . 26) (c . 1) (e . 13) (g♯ . 24) (b♯ . 36) (d♭ . 5) (f . 16) (a . 28) (c♯ . 3) (e♯ . 15) (g♭ . 20) (b♭ . 32) (d . 7) (f♯ . 18) (a♯ . 30) (e♭+ . 12) (g+ . 23) (b♮+ . 35) (d♯+ . 10) (a♭+ . 27) (c+ . 2) (e+ . 14) (g♯+ . 25) (d♭+ . 6) (f+ . 17) (a+ . 29) (c♯+ . 4) (g♭+ . 21) (b♭+ . 33) (d+ . 8) (f♯+ . 19) (a♯+ . 31)))
+
+
+
+
+;; Playing the arciorgano
+
 
 (defun lookup-pitchclass (notename)
+  "Returns the key index of the pitchclass name `notename'."
   (cdr (assoc notename *pitchclass-table*)))
 
-(defun play-pitchclass (notename octave-id duration)
-  (key-on (+ (lookup-pitchclass notename) (* (1- octave-id) 36)) duration))
+(defun play-note (notename duration)
+  "Plays all available octaves of the pitchclass name `notename'."
+  (when notename
+    (let ((pitchclass-index (lookup-pitchclass notename)))
+      (dotimes (i 5)
+        (key-on (+ pitchclass-index (* i 36)) duration)))))
+
+(defun play-shape (origin shape-list duration)
+  "Plays a group of pitchclasses based on `origin' (a pitchclass name) and a list of directions, each of them a pair."
+  (dolist (movement (cons '(0 . 0) shape-list))
+    (play-note (move origin (car movement) (cdr movement)) duration)))
+
+(defparameter *harmonies*
+  '((major ((1 . 0) (0 . 1)))
+    (minor ((0 . 1) (-1 . 1)))
+    (nat-seven ((1 . 0) (0 . 1) (2 . 2)))))
+
+(defun play-harmony (origin harmony duration)
+  "Plays a shape named by `harmony' based on `origin' (pichclass name)."
+  (play-shape origin (second (assoc harmony *harmonies*)) duration))
+
+(defparameter *running* t)
+
+(defun random-element (lst)
+  (nth (random (length lst)) lst))
+
+(defun harmony-walk (origin harmony duration)
+  (when *running*
+    (play-harmony origin harmony duration)
+    (let ((shape (second (assoc harmony *harmonies*))))
+      (at (+ (now) (* 44100 duration))
+          #'harmony-walk
+          (let ((new-origin-delta (random-element shape)))
+            (move origin (car new-origin-delta) (cdr new-origin-delta)))
+          (first (random-element *harmonies*))
+          duration))))
+
+(defun stop-walk ()
+  (setf *running* nil))
+
+(defun start-walk ()
+  (setf *running* t)
+  (harmony-walk 'c 'major 5))
+
+
+
+;; Old and obsolete
+
 
 (defun play-maj (root octave-id duration)
   (mapc (lambda (notename)
@@ -121,7 +221,6 @@
            duration
            play-fun))))
 
-(defparameter *running* t)
 
 (defun play-list (lst step duration &optional last-pitch)
   (when (and *running* lst)
