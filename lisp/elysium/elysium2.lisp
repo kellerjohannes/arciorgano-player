e5130bbeb4ddb2c4d05e7b7da8fe7bf7756e028b
Johannes Keller <johannes.keller@mab-bs.ch>
2023-01-15 13:06:50 +0100

    modified elysium for revival, technical details

--- lisp/elysium/elysium2.lisp
+++ lisp/elysium/elysium2.lisp
@@ -48,29 +48,31 @@
 (defparameter *osc-out* (osc:open :port 5900 :direction :output))
 
 
-(load "~/Coding/pd/arciorgano-pd-sender/lisp/definitions.lisp")
+(load "~/common-lisp/arciorgano-player/lisp/elysium/definitions.lisp")
 
 
 
 (defun parse-tetrachord (origin-name octave tetrachord)
   (let ((start-pitch (+ (* 31 octave) (name->pitch origin-name))))
     (labels ((rec (val lst)
-	       (unless (null lst)
-		 (let ((new-val (- val (interval->pitch (car lst)))))
-		   (cons new-val (rec new-val (rest lst)))))))
+           (unless (null lst)
+         (let ((new-val (- val (interval->pitch (car lst)))))
+           (cons new-val (rec new-val (rest lst)))))))
       (cons start-pitch (rec start-pitch tetrachord)))))
 
 (defun key-off (index)
   (when (< 0 index 147)
     (format t "~a:off " index)
+    (finish-output nil)
     (osc:message *osc-out* "/incudine-bridge" "ii" index 0)))
 
 (defun key-on (index &optional duration-in-sec)
   (when (< 0 index 147)
     (osc:message *osc-out* "/incudine-bridge" "ii" index 1)
     (format t "~a:on " index)
+    (finish-output nil)
     (when duration-in-sec
-      (at (+ (now) #[duration-in-sec sec]) #'key-off index))))
+      (at (+ (now) (* duration-in-sec *sample-rate*)) #'key-off index))))
 
 
 
@@ -90,53 +92,53 @@
 
 (defun play-latest-keyframe (score duration)
   (mapc (lambda (voice)
-	  (play-pitch (first voice) duration)
-	  (when *multiplexer*
-	    (let ((bottom (- 0 (floor *multiplexer* 2))))
-	      (loop for i from bottom to (+ bottom *multiplexer*) do
-		(play-pitch (+ (first voice) (* i 31)) duration)))))
-	score))
+      (play-pitch (first voice) duration)
+      (when *multiplexer*
+        (let ((bottom (- 0 (floor *multiplexer* 2))))
+          (loop for i from bottom to (+ bottom *multiplexer*) do
+        (play-pitch (+ (first voice) (* i 31)) duration)))))
+    score))
 
 (defun make-harmony-server (interval-list)
   (let ((current-list interval-list))
     #'(lambda (&optional selection)
-	(cond ((null current-list) (setf current-list interval-list))
-	      (selection (setf current-list (remove selection current-list)))
-	      (t current-list)))))
+    (cond ((null current-list) (setf current-list interval-list))
+          (selection (setf current-list (remove selection current-list)))
+          (t current-list)))))
 
 (defun find-voiceleading (origin last-note consonance-options)
   (let ((choice (first (sort (mapcar (lambda (interval)
-				       (cons interval (+ origin (interval->pitch interval))))
-				     (funcall consonance-options))
-			     #'< :key (lambda (pitch-candidate)
-					(abs (- last-note (cdr pitch-candidate))))))))
+                       (cons interval (+ origin (interval->pitch interval))))
+                     (funcall consonance-options))
+                 #'< :key (lambda (pitch-candidate)
+                    (abs (- last-note (cdr pitch-candidate))))))))
     (funcall consonance-options (car choice))
     (cdr choice)))
 
 (defun write-to-score (value-list score)
   (cond ((null score) nil)
-	(t (cons (cons (car value-list) (car score))
-		 (write-to-score (rest value-list) (rest score))))))
+    (t (cons (cons (car value-list) (car score))
+         (write-to-score (rest value-list) (rest score))))))
 
 (defun compose-keyframe (tetrachord-position origin model-position score &optional start-harmony)
   (let ((current-pitch (nth tetrachord-position (parse-tetrachord
-						 (car origin)
-						 (cdr origin)
-						 (funcall *tetrachord-generator*)))))
+                         (car origin)
+                         (cdr origin)
+                         (funcall *tetrachord-generator*)))))
     (when start-harmony
       (setf score (write-to-score (cons current-pitch
-					(mapcar (lambda (interval)
-						  (+ current-pitch (interval->pitch interval)))
-						start-harmony))
-				  score)))
+                    (mapcar (lambda (interval)
+                          (+ current-pitch (interval->pitch interval)))
+                        start-harmony))
+                  score)))
     (let ((get-harmony-options (make-harmony-server (nth model-position (funcall *model-generator*)))))
       (write-to-score (cons current-pitch
-			    (mapcar (lambda (voice)
-				      (find-voiceleading current-pitch
-							 (first voice)
-							 get-harmony-options))
-				    (rest score)))
-		      score))))
+                (mapcar (lambda (voice)
+                      (find-voiceleading current-pitch
+                             (first voice)
+                             get-harmony-options))
+                    (rest score)))
+              score))))
 
 (defparameter *playing* t)
 
@@ -152,8 +154,8 @@
 
 (defparameter *duration-generator*
     #'(lambda (&key (reset nil) (factor nil) (rand nil) (rand-range nil))
-	(declare (ignore reset factor rand rand-range))
-	(bpm->sec 42)))
+    (declare (ignore reset factor rand rand-range))
+    (bpm->sec 42)))
 
 
 ;; performance override, safeguard for ninfa ostinato model
@@ -162,9 +164,9 @@
   #'(lambda (&optional next)
       (declare (ignore next))
       '((terza-minore quinta ottava)
-	(terza-maggiore sesta-maggiore ottava)
-	(terza-maggiore sesta-maggiore)
-	(terza-maggiore quinta ottava))))
+    (terza-maggiore sesta-maggiore ottava)
+    (terza-maggiore sesta-maggiore)
+    (terza-maggiore quinta ottava))))
 
 
 
@@ -175,9 +177,9 @@
 (defparameter *duration-generator*
   (let ((internal-speed 45))
     #'(lambda (&key (reset nil) (factor nil) (rand nil) (rand-range nil))
-	(declare (ignore factor rand rand-range))
-	(when reset (setf internal-speed reset))
-	(bpm->sec internal-speed))))
+    (declare (ignore factor rand rand-range))
+    (when reset (setf internal-speed reset))
+    (bpm->sec internal-speed))))
 
 
 
@@ -185,40 +187,40 @@
 
 (defparameter *duration-generator*
   (let ((counter (bpm->sec 45))
-	(internal-factor 1)
-	(random-on 0)
-	(random-range '(1/10 . 1)))
+    (internal-factor 1)
+    (random-on 0)
+    (random-range '(1/10 . 1)))
     #'(lambda (&key (reset nil) (factor nil) (rand nil) (rand-range nil))
-	(when reset (setf counter (bpm->sec reset)))
-	(when factor (setf internal-factor factor))
-	(when rand (setf random-on rand))
-	(when (consp rand-range) (setf random-range rand-range))
-	(let ((result (cond ((zerop random-on)
-			     (setf counter (* counter internal-factor))
-			     (if (not (= internal-factor 1))
-				 (if (< counter (car random-range))
-				     (car random-range)
-				     (if (> counter (cdr random-range))
-					 (cdr random-range)
-					 counter))
-				 counter))
-			    (t (+ (car random-range) (random (* 1.0 (cdr random-range))))))))
-	  (cond ((< result 1/20) (format t "~&min correction") 1/20)
-		((> result 10) (format t "~&max correction") 10)
-		(t result))))))
+    (when reset (setf counter (bpm->sec reset)))
+    (when factor (setf internal-factor factor))
+    (when rand (setf random-on rand))
+    (when (consp rand-range) (setf random-range rand-range))
+    (let ((result (cond ((zerop random-on)
+                 (setf counter (* counter internal-factor))
+                 (if (not (= internal-factor 1))
+                 (if (< counter (car random-range))
+                     (car random-range)
+                     (if (> counter (cdr random-range))
+                     (cdr random-range)
+                     counter))
+                 counter))
+                (t (+ (car random-range) (random (* 1.0 (cdr random-range))))))))
+      (cond ((< result 1/20) (format t "~&min correction") 1/20)
+        ((> result 10) (format t "~&max correction") 10)
+        (t result))))))
 
 (defun loop-tetrachord (position tetrachord origin model score)
   (when *playing*
     (let ((duration (funcall *duration-generator*)))
       (cond ((>= position 4) (loop-tetrachord 0 tetrachord origin model score))
-	    (t (play-latest-keyframe score duration)
-	       (at (+ (now) #[duration sec])
-		   #'loop-tetrachord
-		   (1+ position)
-		   tetrachord
-		   origin
-		   model
-		   (compose-keyframe position origin position score)))))))
+        (t (play-latest-keyframe score duration)
+           (at (+ (now) (* duration *sample-rate*))
+           #'loop-tetrachord
+           (1+ position)
+           tetrachord
+           origin
+           model
+           (compose-keyframe position origin position score)))))))
 
 
 
@@ -241,10 +243,10 @@
 
 (defun play-loop ()
   (loop-tetrachord 0
-		   *tetrachord-generator*
-		   '(g . 1)
-		   *model-generator*
-		   '((50) (58) (68))))
+           *tetrachord-generator*
+           '(g . 1)
+           *model-generator*
+           '((50) (58) (68))))
 
 (defun panic ()
   (my-stop)
@@ -288,13 +290,13 @@
 
 (defun swipe (&key (start 1) (end 146) (delta 1/4) (duration 1/4))
   (cond ((>= start end) nil)
-	(t (key-on start duration)
-	   (at (+ (now) #[delta sec])
-	       #'swipe
-	       :start (1+ start)
-	       :end end
-	       :delta delta
-	       :duration duration))))
+    (t (key-on start duration)
+       (at (+ (now) #[delta sec])
+           #'swipe
+           :start (1+ start)
+           :end end
+           :delta delta
+           :duration duration))))
 
 
 (defun burst-swipe ()
@@ -308,7 +310,7 @@
 (defun burst-random (&key (duration 10) (density 100))
   (loop repeat density do
     (at (+ (now) #[(random (* 1.0 duration)) s])
-	#'key-on (random 146) (random (* 0.5 duration)))))
+    #'key-on (random 146) (random (* 0.5 duration)))))
 
 
 
